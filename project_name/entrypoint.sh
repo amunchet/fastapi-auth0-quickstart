75dfde1ace08a1c7ef27055f27f71a2a6d78b235
Chester Enright <amunchet@gmail.com>
2022-10-04 14:26:08 -0500

    chore: updated entrypoint

--- project_name/entrypoint.sh
+++ project_name/entrypoint.sh
@@ -3,7 +3,11 @@
 # Entrypoint script for docker
 echo "Starting entrypoint.sh..."
 
-echo "Testbed mode"
-
-/src/serve.py
-
+if [ -z "$TESTBED" ]
+then
+    echo "Starting production..."
+    cd /src/ && uvicorn serve:app --workers 4 --port 7000 --host 0.0.0.0
+else
+    echo "Testbed mode"
+    cd /src/ && uvicorn serve:app --reload --port 7000 --host 0.0.0.0
+fi
