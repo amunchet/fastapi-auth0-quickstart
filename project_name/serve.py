7f93d3ffc8acd3fc826ed103924964dc1269baa6
Chester Enright <amunchet@gmail.com>
2022-10-04 14:27:00 -0500

    feat: updates to serve From Flask to FastAPI

--- project_name/serve.py
+++ project_name/serve.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python3
 """
-Sample FastAPI app
+Sample Flask app
 """
 # Permissions scope names
 import functools
 import os
+import json
 
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Response
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from common import auth
-from flask import Flask, request
-from flask_cors import CORS
+
 
 PERM_READ = "read"
 PERM_WRITE = "write"
-PERM_ADMIN = "reports"
+PERM_ADMIN = "Reports"
 
-def error_func(code=401, **args): # pragma: no cover
-    if isinstance(code, int):
-        return "Auth Error", code
-    return "Auth Error", 500
+app = FastAPI()
 
+origins = [
+    "http://localhost:8111",
+    "http://localhost:7008/*"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-requires_auth_read = functools.partial(auth._requires_auth, permission=PERM_READ, error_func=error_func)
-requires_auth_write = functools.partial(auth._requires_auth, permission=PERM_WRITE, error_func=error_func)
-requires_auth_admin = functools.partial(auth._requires_auth, permission=PERM_ADMIN, error_func=error_func)
+requires_auth_read = functools.partial(auth._requires_auth, permission=PERM_READ)
+requires_auth_write = functools.partial(auth._requires_auth, permission=PERM_WRITE)
+requires_auth_admin = functools.partial(auth._requires_auth, permission=PERM_ADMIN)
 
-app = Flask(__name__)
-CORS(app)
 
 
 # Route definitions
 
-@app.route("/insecure")
-def insecure():
-    return "Insecure route.", 200
+@app.get("/insecure")
+def insecure(): # pragma: no cover
+    return "Insecure route."
 
-@app.route("/secure")
+@app.get("/api/secure/secure")
 @requires_auth_read
-def secure():
-    return "Secure route.", 200
+def secure(request: Request): # pragma: no cover
+    """Test for a secure route"""
+    return "Secure route."
 
-
-if __name__ == "__main__": # Run the Flask server in development mode
-    app.debug = True
-    app.config["ENV"] = "development"
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7000)))
