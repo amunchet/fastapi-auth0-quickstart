c525ad990816f1fd2796912d3391ea1f1408a137
Chester Enright <amunchet@gmail.com>
2022-10-04 14:23:04 -0500

    chore: updated auth.py to fastapi

--- project_name/common/auth.py
+++ project_name/common/auth.py
@@ -5,9 +5,14 @@ from functools import wraps, partial
 import json
 from os import environ as env
 from six.moves.urllib.request import urlopen
-from flask import request, _request_ctx_stack
+
+#from flask import request, _request_ctx_stack
+from fastapi import Request, HTTPException
+
 import inspect
 from jose import jwt
+
+
 
 import os
 from dotenv import load_dotenv
@@ -28,24 +33,27 @@ ALGORITHMS = ["RS256"]
 
 
 # Format error response and append status code.
-class AuthError(Exception):
+class AuthError(HTTPException):
     def __init__(self, error, status_code):
-        self.error = error
+        self.detail = error["description"]
         self.status_code = status_code
         self.solution = "Please login again."
 
 
-def get_token_auth_header():
+def get_token_auth_header(request): # pragma: no cover
     """Obtains the access token from the Authorization Header
     """
-    auth = request.headers.get("Authorization", None)
+    if "auth" in request.query_params:
+        auth = request.query_params["auth"]
+    else:
+        auth = request.headers['Authorization']
     if not auth:
         raise AuthError(
             {
                 "code": "authorization_header_missing",
                 "description": "Authorization header is expected",
             },
-            401,
+            405,
         )
 
     parts = auth.split()
@@ -56,7 +64,7 @@ def get_token_auth_header():
                 "code": "invalid_header",
                 "description": "Authorization header must start with" " Bearer",
             },
-            401,
+            406,
         )
     elif len(parts) == 1:
         raise AuthError(
@@ -75,7 +83,7 @@ def get_token_auth_header():
     return token
 
 
-def requires_scope(required_scope):
+def requires_scope(required_scope): # pragma: no cover
     """Determines if the required scope is present in the access token
     Args:
         required_scope (str): The scope required to access the resource
@@ -85,126 +93,121 @@ def requires_scope(required_scope):
     if unverified_claims.get("scope"):
         token_scopes = unverified_claims["scope"].split()
         for token_scope in token_scopes:
-            print(token_scope)
             if token_scope == required_scope:
                 return True
     return False
 
 
-def _requires_auth(f, permission="", error_func=""):
+def _requires_auth(f, permission=""): # pragma: no cover
     """Determines if the access token is valid
     """
 
     @wraps(f)
     def decorated(*args, **kwargs):
+        token = get_token_auth_header(kwargs["request"])
+        jsonurl = urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+        token_scopes = ""
         try:
-            token = get_token_auth_header()
-            jsonurl = urlopen("https://" + AUTH0_DOMAIN + "/.well-known/jwks.json")
-            jwks = json.loads(jsonurl.read())
-            token_scopes = ""
-            try:
-                unverified_header = jwt.get_unverified_header(token)
-            except jwt.JWTError:
-                raise AuthError(
-                    {
-                        "code": "invalid_header",
-                        "description": "Invalid header. "
-                        "Use an RS256 signed JWT Access Token",
-                    },
-                    401,
-                )
-            if unverified_header["alg"] == "HS256":
-                raise AuthError(
-                    {
-                        "code": "invalid_header",
-                        "description": "Invalid header. "
-                        "Use an RS256 signed JWT Access Token",
-                    },
-                    401,
-                )
+            unverified_header = jwt.get_unverified_header(token)
+        except jwt.JWTError:
+            raise AuthError(
+                {
+                    "code": "invalid_header",
+                    "description": "Invalid header. "
+                    "Use an RS256 signed JWT Access Token",
+                },
+                401,
+            )
+        if unverified_header["alg"] == "HS256":
+            raise AuthError(
+                {
+                    "code": "invalid_header",
+                    "description": "Invalid header. "
+                    "Use an RS256 signed JWT Access Token",
+                },
+                401,
+            )
 
-            # Checks for correct permissions that are passed in
-            unverified_claims = jwt.get_unverified_claims(token)
-            if unverified_claims.get("permissions"):
-                token_scopes = unverified_claims["permissions"]
-                found = False
-                for token_scope in token_scopes:
-                    if token_scope == permission:
-                        found = True
-                if not found:
-                    raise AuthError(
-                        {
-                            "code": "invalid_claims",
-                            "description": "Permissions denied (Not found) - " + permission,
-                        },
-                        401,
-                    )
+        # Checks for correct permissions that are passed in
+        unverified_claims = jwt.get_unverified_claims(token)
 
-            else:
+
+        if unverified_claims.get("permissions"):
+            token_scopes = unverified_claims["permissions"]
+            found = False
+            for token_scope in token_scopes:
+                if token_scope == permission:
+                    found = True
+            if not found:
                 raise AuthError(
                     {
                         "code": "invalid_claims",
-                        "description": "Permissions denied (No permission)",
+                        "description": "Permissions denied (Not found) - " + permission,
+                    },
+                    403,
+                )
+
+        else:
+            raise AuthError(
+                {
+                    "code": "invalid_claims",
+                    "description": "Permissions denied (No permission)",
+                },
+                403,
+            )
+
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"],
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_IDENTIFIER,
+                    issuer="https://" + AUTH0_DOMAIN + "/",
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError(
+                    {"code": "token_expired", "description": "token is expired"}, 401
+                )
+            except jwt.JWTClaimsError:
+                raise AuthError(
+                    {
+                        "code": "invalid_claims",
+                        "description": "incorrect claims,"
+                        " please check the audience and issuer",
+                    },
+                    407,
+                )
+            except Exception:
+                raise AuthError(
+                    {
+                        "code": "invalid_header",
+                        "description": "Unable to parse authentication" " token.",
                     },
                     401,
                 )
 
-            rsa_key = {}
-            for key in jwks["keys"]:
-                if key["kid"] == unverified_header["kid"]:
-                    rsa_key = {
-                        "kty": key["kty"],
-                        "kid": key["kid"],
-                        "use": key["use"],
-                        "n": key["n"],
-                        "e": key["e"],
-                    }
-            if rsa_key:
-                try:
-                    payload = jwt.decode(
-                        token,
-                        rsa_key,
-                        algorithms=ALGORITHMS,
-                        audience=API_IDENTIFIER,
-                        issuer="https://" + AUTH0_DOMAIN + "/",
-                    )
-                except jwt.ExpiredSignatureError:
-                    """
-                    raise AuthError(
-                        {"code": "token_expired", "description": "token is expired"}, 401
-                    )
-                    """
-                    return error_func(code=471, *args, **kwargs)
-                except jwt.JWTClaimsError:
-                    raise AuthError(
-                        {
-                            "code": "invalid_claims",
-                            "description": "incorrect claims,"
-                            " please check the audience and issuer",
-                        },
-                        401,
-                    )
-                except Exception:
-                    raise AuthError(
-                        {
-                            "code": "invalid_header",
-                            "description": "Unable to parse authentication" " token.",
-                        },
-                        401,
-                    )
+            # _request_ctx_stack.top.current_user = payload
+            if (
+                "scopes" in inspect.signature(f).parameters
+            ):  # Passes out the scopes if the decorated function has it as an argument
+                kwargs["scopes"] = token_scopes
 
-                _request_ctx_stack.top.current_user = payload
-                if (
-                    "scopes" in inspect.signature(f).parameters
-                ):  # Passes out the scopes if the decorated function has it as an argument
-                    kwargs["scopes"] = token_scopes
-
-                return f(*args, **kwargs)
-            raise AuthError(
-                {"code": "invalid_header", "description": "Unable to find appropriate key"},
-                401,
-            )
-        except AuthError as exc:
-            return error_func(msg=exc.error, code=exc.status_code, *args, **kwargs)
+            return f(*args, **kwargs)
+        raise AuthError(
+            {"code": "invalid_header", "description": "Unable to find appropriate key"},
+            401,
+        )
 
     return decorated
